f2ed43bc53cc80dbebcd6e32af490640d43a59cf
LuiggySilva <luiggy.silva@ccc.ufcg.edu.br>
2023-04-10 13:35:47 -0300

    Criando repositório com os arquivos dos projetos

---
+++ Projeto02/src/testes/MainTest.java
@@ -0,0 +1,14 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
