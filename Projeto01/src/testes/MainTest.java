5912f91c8d98f28cc43db57320804e287e88a5f9
gabrielmax.predialize <gabriel.max@predialize.com.br>
2023-04-12 16:16:08 -0300

    fix: changing name of class to fix the bug

--- Projeto01/src/testes/MainTest.java
+++ Projeto01/src/testes/MainTest.java
@@ -10,7 +10,11 @@ import java.text.SimpleDateFormat;
 
 import org.junit.jupiter.api.Test;
 
-class Tests {
+import projeto.Client;
+import projeto.Filter;
+import projeto.Invoice;
+
+class MainTest {
 
 	@Test
 	public void lessThen2000() throws ParseException {
@@ -18,9 +22,9 @@ class Tests {
 		
 		Invoice[] invoices = new Invoice[3];
 		
-		Invoice i1c1 = new Invoice(1, 1000, "01/02/2023", c1); // Invoice (code, value, date, client)
-		Invoice i2c1 = new Invoice(2, 2000, "02/02/2023", c1);
-		Invoice i3c1 = new Invoice(3, 3000, "03/02/2023", c1);
+		Invoice i1c1 = new Invoice(1, 1000.00, "01/02/2023", c1); // Invoice (code, value, date, client)
+		Invoice i2c1 = new Invoice(2, 2000.00, "02/02/2023", c1);
+		Invoice i3c1 = new Invoice(3, 3000.00, "03/02/2023", c1);
 		
 		invoices[0] = i1c1;
 		invoices[1] = i2c1;
@@ -42,15 +46,15 @@ class Tests {
 		
 		Invoice[] invoices = new Invoice[9];
 		
-		Invoice i1c1 = new Invoice(1, 2250, getUpdatedMonthsOfTodayToString(-2), c1); // Invoice (code, value, date, client)
-		Invoice i2c1 = new Invoice(2, 2250, getUpdatedMonthsOfTodayToString(-1), c1);
-		Invoice i3c1 = new Invoice(3, 2250, getUpdatedDaysOfTodayToString(-2), c1);
-		Invoice i4c1 = new Invoice(4, 2250, getUpdatedDaysOfTodayToString(-1), c1);
-		Invoice i5c1 = new Invoice(5, 2250, getToday(), c1);
-		Invoice i6c1 = new Invoice(6, 2250, getUpdatedDaysOfTodayToString(1), c1);
-		Invoice i7c1 = new Invoice(7, 2250, getUpdatedDaysOfTodayToString(2), c1);
-		Invoice i8c1 = new Invoice(8, 2250, getUpdatedMonthsOfTodayToString(1), c1);
-		Invoice i9c1 = new Invoice(9, 2250, getUpdatedMonthsOfTodayToString(2), c1);
+		Invoice i1c1 = new Invoice(1, 2250.00, getUpdatedMonthsOfTodayToString(-2), c1); // Invoice (code, value, date, client)
+		Invoice i2c1 = new Invoice(2, 2250.00, getUpdatedMonthsOfTodayToString(-1), c1);
+		Invoice i3c1 = new Invoice(3, 2250.00, getUpdatedDaysOfTodayToString(-2), c1);
+		Invoice i4c1 = new Invoice(4, 2250.00, getUpdatedDaysOfTodayToString(-1), c1);
+		Invoice i5c1 = new Invoice(5, 2250.00, getToday(), c1);
+		Invoice i6c1 = new Invoice(6, 2250.00, getUpdatedDaysOfTodayToString(1), c1);
+		Invoice i7c1 = new Invoice(7, 2250.00, getUpdatedDaysOfTodayToString(2), c1);
+		Invoice i8c1 = new Invoice(8, 2250.00, getUpdatedMonthsOfTodayToString(1), c1);
+		Invoice i9c1 = new Invoice(9, 2250.00, getUpdatedMonthsOfTodayToString(2), c1);
 		
 		invoices[0] = i1c1;
 		invoices[1] = i2c1;
@@ -83,15 +87,15 @@ class Tests {
 		
 		Invoice[] invoices = new Invoice[9];
 		
-		Invoice i1c1 = new Invoice(1, 2750, getUpdatedMonthsOfTodayToString(-3), c1); // Invoice (code, value, date, client)
-		Invoice i2c1 = new Invoice(2, 2750, getUpdatedMonthsOfTodayToString(-2), c1);
-		Invoice i3c1 = new Invoice(3, 2750, getUpdatedDaysOfTodayToString(-2), c1);
-		Invoice i4c1 = new Invoice(4, 2750, getUpdatedDaysOfTodayToString(-1), c1);
-		Invoice i5c1 = new Invoice(5, 2750, getToday(), c1);
-		Invoice i6c1 = new Invoice(6, 2750, getUpdatedDaysOfTodayToString(1), c1);
-		Invoice i7c1 = new Invoice(7, 2750, getUpdatedDaysOfTodayToString(2), c1);
-		Invoice i8c1 = new Invoice(8, 2750, getUpdatedMonthsOfTodayToString(2), c1);
-		Invoice i9c1 = new Invoice(9, 2750, getUpdatedMonthsOfTodayToString(3), c1);
+		Invoice i1c1 = new Invoice(1, 2750.00, getUpdatedMonthsOfTodayToString(-3), c1); // Invoice (code, value, date, client)
+		Invoice i2c1 = new Invoice(2, 2750.00, getUpdatedMonthsOfTodayToString(-2), c1);
+		Invoice i3c1 = new Invoice(3, 2750.00, getUpdatedDaysOfTodayToString(-2), c1);
+		Invoice i4c1 = new Invoice(4, 2750.00, getUpdatedDaysOfTodayToString(-1), c1);
+		Invoice i5c1 = new Invoice(5, 2750.00, getToday(), c1);
+		Invoice i6c1 = new Invoice(6, 2750.00, getUpdatedDaysOfTodayToString(1), c1);
+		Invoice i7c1 = new Invoice(7, 2750.00, getUpdatedDaysOfTodayToString(2), c1);
+		Invoice i8c1 = new Invoice(8, 2750.00, getUpdatedMonthsOfTodayToString(2), c1);
+		Invoice i9c1 = new Invoice(9, 2750.00, getUpdatedMonthsOfTodayToString(3), c1);
 		
 		invoices[0] = i1c1;
 		invoices[1] = i2c1;
@@ -124,11 +128,11 @@ class Tests {
 		
 		Invoice[] invoices = new Invoice[5];
 		
-		Invoice i1c1 = new Invoice(1, 3500, getUpdatedMonthsOfTodayToString(-3), c1); // Invoice (code, value, date, client)
-		Invoice i2c1 = new Invoice(2, 3999, getUpdatedMonthsOfTodayToString(-2), c1);
-		Invoice i3c1 = new Invoice(3, 4000, getUpdatedDaysOfTodayToString(-2), c1);
-		Invoice i4c1 = new Invoice(4, 4001, getUpdatedDaysOfTodayToString(-1), c1);
-		Invoice i5c1 = new Invoice(5, 5000, getToday(), c1);
+		Invoice i1c1 = new Invoice(1, 3500.00, getUpdatedMonthsOfTodayToString(-3), c1); // Invoice (code, value, date, client)
+		Invoice i2c1 = new Invoice(2, 3999.99, getUpdatedMonthsOfTodayToString(-2), c1);
+		Invoice i3c1 = new Invoice(3, 4000.00, getUpdatedDaysOfTodayToString(-2), c1);
+		Invoice i4c1 = new Invoice(4, 4000.01, getUpdatedDaysOfTodayToString(-1), c1);
+		Invoice i5c1 = new Invoice(5, 5000.00, getToday(), c1);
 		
 		invoices[0] = i1c1;
 		invoices[1] = i2c1;
@@ -154,15 +158,15 @@ class Tests {
 		
 		Invoice[] invoices = new Invoice[8];
 		
-		Invoice i1c1 = new Invoice(1, 3500, "01/02/2023", c1); // Invoice (code, value, date, client)
-		Invoice i2c1 = new Invoice(2, 3999, "01/02/2023", c1);
-		Invoice i3c1 = new Invoice(3, 4000, "01/02/2023", c1);
-		Invoice i4c1 = new Invoice(4, 4001, "01/02/2023", c1);
-		Invoice i5c1 = new Invoice(5, 5000, "01/02/2023", c1);
+		Invoice i1c1 = new Invoice(1, 3500.00, "01/02/2023", c1); // Invoice (code, value, date, client)
+		Invoice i2c1 = new Invoice(2, 3999.99, "01/02/2023", c1);
+		Invoice i3c1 = new Invoice(3, 4000.00, "01/02/2023", c1);
+		Invoice i4c1 = new Invoice(4, 4000.01, "01/02/2023", c1);
+		Invoice i5c1 = new Invoice(5, 5000.00, "01/02/2023", c1);
 		
-		Invoice i1c2 = new Invoice(1, 1000, "01/02/2023", c2);
-		Invoice i2c2 = new Invoice(2, 5000, "01/02/2023", c2);
-		Invoice i3c2 = new Invoice(3, 3500, "01/02/2023", c2);
+		Invoice i1c2 = new Invoice(1, 1000.00, "01/02/2023", c2);
+		Invoice i2c2 = new Invoice(2, 5000.00, "01/02/2023", c2);
+		Invoice i3c2 = new Invoice(3, 3500.00, "01/02/2023", c2);
 		
 		invoices[0] = i1c1;
 		invoices[1] = i2c1;
@@ -188,7 +192,7 @@ class Tests {
 	}
 	
 	@Test
-	public void noInvoice() throws ParseException {
+	public void noInvoice() throws ParseException {	
 		Invoice[] invoices = new Invoice[3];
 						
 		Invoice[] expectedInvoices = new Invoice[3];
